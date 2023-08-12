e4dae3fa3e5d607936e6ab5f215fa4b796fd896e
Jed <adijed3@gmail.com>
2023-08-12 14:37:01 +0800

    first commit for django version compatability

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-sage-streaming',
+    name='fork-django-streaming',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
-    version='0.1.1',
+    version='0.2.0',
     license='GNU',
     description='video streaming based on Django',
-    author='Sage Team',
-    author_email='mail@sageteam.org',
-    url='https://github.com/sageteam-org/django-sage-streaming',
-    download_url='https://github.com/sageteam-org/django-sage-streaming/archive/refs/tags/0.1.0.tar.gz',
+    author='Jed',
+    author_email='adijed3@gmail.com',
+    url='https://github.com/J3dd4/fork-django-streaming',
+    # download_url='https://github.com/sageteam-org/django-sage-streaming/archive/refs/tags/0.1.0.tar.gz',
     keywords=['django', 'python', 'streaming', 'video streaming'],
     install_requires=[
         'Django',
